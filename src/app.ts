600410bfba19f6057cec9454dde668f6ff88abad
Guyddo <guyddogl@gmail.com>
2023-02-14 13:24:17 -0300

    Add motorcycles route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import carRoutes from './routes/carRoutes';
+import motorcyclesRoutes from './routes/motorcycleRoutes';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', carRoutes);
+app.use('/motorcycles', motorcyclesRoutes);
 
 export default app;
