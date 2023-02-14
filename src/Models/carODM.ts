5c7167dbec9eed705bc982c94050d1ff301fc508
Guyddo <guyddogl@gmail.com>
2023-02-14 13:44:14 -0300

    Refactor carODM and carService to extend abstractODM

--- src/Models/carODM.ts
+++ src/Models/carODM.ts
@@ -1,12 +1,11 @@
-import { Model, Schema, model, models, isValidObjectId } from 'mongoose';
+import { Schema, model, models } from 'mongoose';
 import ICar from '../Interfaces/ICar';
+import AbstractODM from './abstractODM';
 
-class CarODM {
-  private schema: Schema;
-  private model: Model<ICar>;
-
+class CarODM extends AbstractODM<ICar> {
   constructor() {
-    this.schema = new Schema<ICar>({
+    super();
+    super.schema = new Schema<ICar>({
       model: { type: String, required: true },
       year: { type: Number, required: true },
       color: { type: String, required: true },
@@ -16,24 +15,6 @@ class CarODM {
       seatsQty: { type: Number, required: true },
     });
     this.model = models.Car || model('Car', this.schema);
-  }
-
-  public async createNewCar(car: ICar): Promise<ICar> { return this.model.create({ ...car }); }
-
-  public async getAllCars(): Promise<ICar[]> { return this.model.find(); }
-
-  public async getCarByID(id: string): Promise<ICar[] | undefined> {
-    if (!isValidObjectId(id)) return undefined;
-    return this.model.find({ _id: id });
-  }
-
-  public async updateCar(id: string, car: ICar) {
-    if (!isValidObjectId(id)) return undefined;
-    return this.model.findByIdAndUpdate(
-      { _id: id },
-      { ...car },
-      { new: true },
-    );
   }
 }
 
--- src/Services/carService.ts
+++ src/Services/carService.ts
@@ -7,26 +7,26 @@ class CarService {
 
   public async createNewCar(car: ICar) { 
     const carODM = new CarODM();
-    const newCar = await carODM.createNewCar(car);
+    const newCar = await carODM.createNewVehicle(car);
     return this.createNewCarDomain(newCar);
   }
 
   public async getAllCars() {
     const carODM = new CarODM();
-    const cars = await carODM.getAllCars();
+    const cars = await carODM.getAllVehicles();
     return cars.map((e) => this.createNewCarDomain(e));
   }
 
   public async getCarByID(id: string) {
     const carODM = new CarODM();
-    const cars = await carODM.getCarByID(id);
+    const cars = await carODM.getVehicleById(id);
     if (cars === undefined) return undefined;
     return cars.map((e: ICar) => this.createNewCarDomain(e));
   }
 
   public async updateCar(id: string, car: ICar) {
     const carODM = new CarODM();
-    const newCar = await carODM.updateCar(id, car);
+    const newCar = await carODM.updateVehicle(id, car);
     if (newCar === undefined) return undefined;
     if (newCar === null) return null;
     return this.createNewCarDomain(newCar);
