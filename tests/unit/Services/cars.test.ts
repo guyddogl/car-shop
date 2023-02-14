1b30e2b2d794663bee02498a1a92b6e867acc103
Guyddo <guyddogl@gmail.com>
2023-02-13 22:06:18 -0300

    Create car tests

---
+++ tests/unit/Services/cars.test.ts
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+import Car from '../../../src/Domains/Car';
+import CarsService from '../../../src/Services/carService';
+import { mockCars, mockCar } from './carsMock';
+
+describe('Testes Cars Services', function () {
+  afterEach(function () { sinon.restore(); });
+
+  it('Verifica se é possível adicionar um carro', async function () {
+    const car: Car = new Car({ id: '6348513f34c397abcad040b2', ...mockCar });
+
+    sinon.stub(Model, 'create').resolves(car);
+
+    const service = new CarsService();
+    const result = await service.createNewCar(mockCar);
+
+    expect(result).to.be.deep.equal(car);    
+  });
+
+  it('Verifica se retorna todos os carros', async function () {
+    const car: Car[] = mockCars.map((e) => new Car(e));
+
+    sinon.stub(Model, 'find').resolves(mockCars);
+
+    const service = new CarsService();
+    const result = await service.getAllCars();
+
+    expect(result).to.be.deep.equal(car);    
+  });
+
+  it('Verifica se encontra o carro pelo ID', async function () {
+    const car: Car = new Car({ ...mockCars[0] });
+
+    sinon.stub(Model, 'find').resolves([mockCars[0]]);
+
+    const service = new CarsService();
+    const result = await service.getCarByID('6348513f34c397abcad040b2');
+
+    expect(result).to.be.deep.equal([car]);    
+  });
+});
---
+++ tests/unit/Services/carsMock.ts
@@ -0,0 +1,33 @@
+import ICars from '../../../src/Interfaces/ICar';
+
+export const mockCars: ICars[] = [
+  {
+    id: '63eada69945a9369ec7df41e',
+    model: 'Fusion',
+    year: 2017,
+    color: 'White',
+    status: true,
+    buyValue: 99.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '63ead9db945a9369ec7df416',
+    model: 'Magentis',
+    year: 1995,
+    color: 'Black',
+    buyValue: 39,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+];
+
+export const mockCar: ICars = {
+  id: '63eada69945a9369ec7df41e',
+  model: 'Fusion',
+  year: 2017,
+  color: 'White',
+  buyValue: 99.99,
+  doorsQty: 4,
+  seatsQty: 5,
+};
